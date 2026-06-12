44d0f0454adfe5437b6b81608eea781c0b29212d
Varshini V K <varshini58varsh@gmail.com>
2026-06-12 04:42:00 +0000

    Added zerotoend.java

---
+++ zerotoend.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+class zerotoend {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[count++] = arr[i];
+            }
+        }
+        while (count < n) {
+            arr[count++] = 0;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        
+    }
+}                              
